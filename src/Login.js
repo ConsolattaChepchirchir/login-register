d527afe0605a72a9a3909a6ef4836c97b64c6745
ConsolattaChepchirchir <consochep97@gmail.com>
2023-05-04 11:47:30 +0300

    first commit

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+export const Login =(props) =>{
+    const [Email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(Email);
+    }
+    return(
+        
+        <div className="auth-form-container">
+            <h1>ToTu Event</h1>
+    <form className="login-form"onSubmit={handleSubmit}>
+        <label for="Email">Email</label>
+        <input value={Email} onChange={(e)=> setEmail(e.target.value)}type="Email" placeholder="youremail@gmail.com"/>
+        <label for="Password">Password</label>
+        <input value={pass} onChange={(e)=> setPass(e.target.value)}type="Password" placeholder="*********"/>
+        <button type="submit">Login</button>
+    </form>
+    <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register Here</button>
+    </div>
+    )
+}
---
+++ src/Register.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+export const Register =(props) =>{
+    const [Email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name,setName] = useState('')
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(Email);
+    }
+    return(
+        <div className="auth-form-container">
+            <h2>Register</h2>
+    <form className="register-form"onSubmit={handleSubmit}>
+        <label htmlfor="name">Full Name</label>
+        <input value={name} name="name" paceholder="Full Name"/>
+        <lable for="Email">Email</lable>
+        <input value={Email} onChange={(e)=> setEmail(e.target.value)}type="Email" placeholder="youremail@gmail.com"/>
+        <label for="Password">Password</label>
+        <input value={pass} onChange={(e)=> setPass(e.target.value)}type="Password" placeholder="*********"/>
+        <button type="submit">Login</button>
+    </form>
+    <button className="link-btn"onClick={() => props.onFormSwitch('Login')}>Don't have an account? Register Here</button>
+    </div>
+    )
+}
